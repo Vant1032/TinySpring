86fe8ac7ba41ebf42c67cf13e934475064cf509f
ZengXiaoBin <forzengxiaobin@163.com>
2018-08-03 01:38:25 +0800

    基本功能已实现,未测试

--- src/main/java/core/annotations/ComponentScan.java
+++ src/main/java/core/annotations/ComponentScan.java
@@ -1,9 +1,16 @@
 package core.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author Vant
  * @version 2018/8/3 上午 12:44
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface ComponentScan {
     Class[] basePackages();
 }
