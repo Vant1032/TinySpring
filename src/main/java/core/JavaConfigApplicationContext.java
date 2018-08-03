d01405e0df9283605ff8f71c99c3de5965f5f500
ZengXiaoBin <forzengxiaobin@163.com>
2018-08-03 09:36:46 +0800

    正在添加scope,autowired支持,新增了一些注解

--- src/main/java/core/JavaConfigApplicationContext.java
+++ src/main/java/core/JavaConfigApplicationContext.java
@@ -3,8 +3,12 @@ package core;
 
 import core.annotations.Bean;
 import core.annotations.ComponentScan;
+import core.annotations.Configuration;
+import core.annotations.Scope;
+import core.annotations.ScopeType;
 import core.util.SearchPackageClassUtil;
 
+import java.lang.annotation.Annotation;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,13 +17,20 @@ import java.util.Map;
  * @version 2018/8/3 上午 12:41
  */
 public class JavaConfigApplicationContext implements ApplicationContext {
-    Map<String, Class> beanMap = new HashMap<>();
+    private Map<String, Class> beanMap = new HashMap<>();
+    private Map<String, Object> singletonBeanCache = new HashMap<>();
+
 
     /**
      * 由于只支持Bean注解,所以这样处理
+     *
      * @param config javaConfig类,用于配置TinySpring的类
      */
     public JavaConfigApplicationContext(Class config) {
+        if (config.getAnnotation(Configuration.class) == null) {
+            return;
+        }
+
         ComponentScan componentScan = (ComponentScan) config.getDeclaredAnnotation(ComponentScan.class);
         if (componentScan != null) {
             Class[] basePackages = componentScan.basePackages();
@@ -34,6 +45,7 @@ public class JavaConfigApplicationContext implements ApplicationContext {
                         if (beanAnnotation != null) {
                             beanMap.put(beanAnnotation.value(), aClass);
                         }
+                        //TODO:添加scope的单例初始化缓存
                     } catch (Throwable e) {
                         e.printStackTrace();
                     }
@@ -48,7 +60,12 @@ public class JavaConfigApplicationContext implements ApplicationContext {
     @Override
     public Object getBean(String beanName) {
         try {
-            return beanMap.get(beanName).newInstance();//只支持具有无参构造器的bean
+            Class beanClass = beanMap.get(beanName);
+            Scope scope = (Scope) beanClass.getAnnotation(Scope.class);
+            if (scope != null && scope.value() == ScopeType.Singleton) {
+
+            }
+            return beanClass.newInstance();//只支持具有无参构造器的bean
         } catch (InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         }
