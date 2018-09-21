096fb1ed0ac7f27cdcd8f0a7b1433f125e5fd482
Vant <forvant@qq.com>
2018-09-22 00:31:26 +0800

    save state

--- tinyspring-webmvc/src/main/java/cc/vant/tinyspring/webmvc/DispacherServlet.java
+++ tinyspring-webmvc/src/main/java/cc/vant/tinyspring/webmvc/DispacherServlet.java
@@ -1,10 +1,26 @@
 package cc.vant.tinyspring.webmvc;
 
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author Vant
  * @since 2018/8/31 22:53
  */
-public class DispacherServlet {
+public class DispacherServlet extends HttpServlet {
 
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
